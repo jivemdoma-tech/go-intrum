056e297263888e35676862632a8f607cacea016d
theloveofcat <theloveofcat.official@gmail.com>
2025-03-13 13:36:40 +0300

    update sales get by change stage method

--- sales_getbychangestage.go
+++ sales_getbychangestage.go
@@ -15,33 +15,31 @@ type SalesGetByChangeStageParams struct {
 }
 
 // Ссылка на метод: https://www.intrumnet.com/api/#sales-filter-stage-period
-func SalesGetByChangeStage(ctx context.Context, subdomain, apiKey string, timeoutSec int, inputParams *SalesGetByChangeStageParams) (*SalesGetByChangeStageResponse, error) {
-	var (
-		primaryURL string = fmt.Sprintf("http://%s.intrumnet.com:81/sharedapi/sales/getbychangestage", subdomain)
-		backupURL  string = fmt.Sprintf("http://%s.intrumnet.com:80/sharedapi/sales/getbychangestage", subdomain)
-	)
+func SalesGetByChangeStage(ctx context.Context, subdomain, apiKey string, inputParams *SalesGetByChangeStageParams) (*SalesGetByChangeStageResponse, error) {
+	methodURL := fmt.Sprintf("http://%s.intrumnet.com:81/sharedapi/sales/getbychangestage", subdomain)
 
 	// Параметры запроса
 
-	params := make(map[string]string, getParamsSize(inputParams))
+	params := make(map[string]string, 2+len(inputParams.SaleID)+len(inputParams.Stage))
 
 	// date_start
-	params["params[date_start]"] = inputParams.DateStart.Format(dateLayout)
+	if !inputParams.DateStart.IsZero() {
+		params["params[date_start]"] = inputParams.DateStart.Format(dateLayout)
+	}
 	// date_end
-	params["params[date_end]"] = inputParams.DateEnd.Format(dateLayout)
+	if !inputParams.DateEnd.IsZero() {
+		params["params[date_end]"] = inputParams.DateEnd.Format(dateLayout)
+	}
 	// sale_id
-	addSliceToParams(params, "sale_id", inputParams.SaleID)
+	addSliceToParams("sale_id", params, inputParams.SaleID)
 	// stage
-	addSliceToParams(params, "stage", inputParams.Stage)
+	addSliceToParams("stage", params, inputParams.Stage)
 
 	// Получение ответа
 
 	var resp SalesGetByChangeStageResponse
-
-	if err := rawRequest(ctx, primaryURL, apiKey, timeoutSec, params, &resp); err != nil {
-		if err := rawRequest(ctx, backupURL, apiKey, timeoutSec, params, &resp); err != nil {
-			return nil, err
-		}
+	if err := rawRequest(ctx, apiKey, methodURL, params, &resp); err != nil {
+		return nil, err
 	}
 
 	return &resp, nil
