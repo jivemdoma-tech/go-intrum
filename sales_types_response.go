cd65d5e64b7d55bf5f8a6ce1f9a79b6eb40150ae
theloveofcat <theloveofcat.official@gmail.com>
2025-03-13 15:15:56 +0300

    update sales types method response struct

--- sales_types_response.go
+++ sales_types_response.go
@@ -5,15 +5,15 @@ type SalesTypesResponse struct {
 	Data   []*SalesTypesData `json:"data"`
 }
 type SalesTypesData struct {
-	ID     string        `json:"id"`
+	ID     uint16        `json:"id,string"`
 	Name   string        `json:"name"`
 	Stages []*SalesStage `json:"stages"`
 }
 type SalesStage struct {
-	ID        string `json:"id"`
+	ID        uint16 `json:"id,string"`
 	Name      string `json:"name"`
 	Color     string `json:"color"`
-	Order     string `json:"order"`
-	IsSuccess *bool  `json:"is_success,omitempty"`
-	IsFail    *bool  `json:"is_fail,omitempty"`
+	Order     uint16 `json:"order,string"`
+	IsSuccess *bool   `json:"is_success,omitempty"`
+	IsFail    *bool   `json:"is_fail,omitempty"`
 }
