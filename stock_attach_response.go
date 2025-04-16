d5b465c7ebf7a3bb5bc802551e40db8273f9dca7
theloveofcat <theloveofcat.official@gmail.com>
2025-04-16 19:58:03 +0300

    Add custom UnmarshalJSON to avoid errors

--- stock_attach_response.go
+++ stock_attach_response.go
@@ -1,5 +1,7 @@
 package gointrum
 
+import "encoding/json"
+
 type StockAttachResponse struct {
 	*Response
 	Data map[string]*StockAttachData `json:"data,omitempty"`
@@ -7,4 +9,33 @@ type StockAttachResponse struct {
 
 type StockAttachData struct {
 	Requests []string `json:"requests"`
+}
+
+func (s *StockAttachResponse) UnmarshalJSON(data []byte) error {
+	type Alias StockAttachResponse
+
+	// Временная структура с raw json для поля Data
+	aux := &struct {
+		Data json.RawMessage `json:"data"`
+		*Alias
+	}{
+		Alias: (*Alias)(s),
+	}
+
+	// Парсим все кроме поля Data
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	// Обработка поля Data
+	if string(aux.Data) == "[]" {
+		s.Data = nil
+	} else {
+		// обычный парсинг в map
+		if err := json.Unmarshal(aux.Data, &s.Data); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
