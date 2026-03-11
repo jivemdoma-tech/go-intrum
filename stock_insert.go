cd9a580856a1076befaaa1899fa516daae26f82e
sergek364 <sergekalitvintsev.364@gmail.com>
2026-03-11 17:26:30 +0300

    Complete stock insert

--- stock_insert.go
+++ stock_insert.go
@@ -6,6 +6,11 @@ import (
 	"strconv"
 )
 
+// TODO: Реализовать оставшиеся поля StockInsertParams.
+//  GroupID
+//  Copy
+// TODO: Реализовать в StockInsertParams.Fields изменение полей типов: attach
+
 // StockInsert - добавление объекта в CRM. Документация: https://www.intrumnet.com/api/#stock-insert
 func StockInsert(ctx context.Context, subdomain, apiKey string, p *StockInsertParams) (*StockInsertResponse, error) {
 	methodURL := fmt.Sprintf("http://%s.intrumnet.com:81/sharedapi/stock/insert", subdomain)
@@ -19,7 +24,7 @@ func StockInsert(ctx context.Context, subdomain, apiKey string, p *StockInsertPa
 	}
 
 	// Обязательные поля
-	if p.Type <= 0 {
+	if p.Category <= 0 {
 		return nil, newErrEmptyRequiredParams(methodURL)
 	}
 
@@ -32,95 +37,143 @@ func StockInsert(ctx context.Context, subdomain, apiKey string, p *StockInsertPa
 	return resp, nil
 }
 
-// StockInsertParams - параметры запроса.
+// =====================================================================================================================
+// Request
+// =====================================================================================================================
+
+// StockInsertParams - параметры запроса StockInsert.
 //
 // Обязательные поля:
-//   - Type
+//   - Category
 //
 // Основные параметры запроса:
-//   - Type: ID типа объекта.
-//   - Name: Название объекта.
-//   - Manager: ID главного ответственного.
-//   - AdditionalManagers: Массив ID доп. ответственных.
-//   - RelatedWithCustomer: ID контакта, прикрепленного к объекту.
-//   - Fields: массив ID полей и значений.
+//   - Category:            Id категории объекта.
+//   - Name:                Название объекта.
+//   - Manager:             Id главного ответственного.
+//   - AdditionalManagers:  Массив id доп. ответственных.
+//   - RelatedWithCustomer: Id контакта, прикрепленного к объекту.
+//   - Fields:              Массив ID полей и значений.
 //     Для типа (multiselect) возможно указывать несколько вариантов: "{ЗНАЧЕНИЕ},{ЗНАЧЕНИЕ},{ЗНАЧЕНИЕ}".
 type StockInsertParams struct {
-	Type                int64   // ID типа объекта.
+	Category            int64   // Id категории объекта.
 	Name                string  // Название объекта.
-	Manager             int64   // ID главного ответственного.
-	AdditionalManagers  []int64 // Массив ID доп. ответственных.
-	RelatedWithCustomer int64   // ID контакта, прикрепленного к объекту.
-	// Fields: массив ID полей и значений.
+	Manager             int64   // Id главного ответственного.
+	AdditionalManagers  []int64 // Массив id доп. ответственных.
+	RelatedWithCustomer int64   // Id контакта, прикрепленного к объекту.
+	// Fields: массив id полей и значений.
 	//
 	// Для типа (multiselect) возможно указывать несколько вариантов:
 	//  "{ЗНАЧЕНИЕ},{ЗНАЧЕНИЕ},{ЗНАЧЕНИЕ}".
 	Fields      map[int64]string
-	FieldsPoint map[int64]Point    // Аналогично Fields для типа "point".
+	FieldsPoint map[int64]*Point   // Аналогично Fields для типа "point".
 	FieldsFile  map[int64][]string // Аналогично Fields для типа "file".
-
-	// TODO: Оставшиеся поля. При реализации полей адаптируйте выделение памяти для paramsMap в методе params.
-	//  GroupID
-	//  Copy
 }
 
 // params возвращает параметры запроса в формате map[string]string (с эффективным выделением памяти).
 func (p StockInsertParams) params() map[string]string {
 	// Выделение памяти
-	filesCount := 0
+	size := 5 // Поля с простыми типами
+	size += len(p.AdditionalManagers)
+	size += len(p.Fields) * 2
+	size += len(p.FieldsPoint) * 3
 	for _, files := range p.FieldsFile {
-		filesCount += len(files)
+		size += len(files) * 2
 	}
-	paramsMap := make(map[string]string,
-		// Единичные поля
-		4+
-			// Слайсы
-			len(p.AdditionalManagers)+
-			// Мапы
-			len(p.Fields)*2+
-			len(p.FieldsPoint)*3+
-			filesCount*2,
-	)
+	paramsMap := make(map[string]string, size)
 
 	// parent
-	paramsMap["params[0][parent]"] = strconv.FormatInt(p.Type, 10)
+	if v := p.Category; v > 0 {
+		paramsMap["params[0][parent]"] = strconv.FormatInt(v, 10)
+	}
 	// name
-	if p.Name != "" {
-		paramsMap["params[0][name]"] = p.Name
+	if v := p.Name; v != "" {
+		paramsMap["params[0][name]"] = v
 	}
 	// author
-	if p.Manager != 0 {
-		paramsMap["params[0][author]"] = strconv.FormatInt(p.Manager, 10)
+	if v := p.Manager; v > 0 {
+		paramsMap["params[0][author]"] = strconv.FormatInt(v, 10)
 	}
 	// additional_author
-	for i, v := range p.AdditionalManagers {
-		paramsMap[fmt.Sprintf("params[0][additional_author][%d]", i)] = strconv.FormatInt(v, 10)
+	for i, id := range p.AdditionalManagers {
+		if id > 0 {
+			k, v := fmt.Sprintf("params[0][additional_author][%d]", i), strconv.FormatInt(id, 10)
+			paramsMap[k] = v
+		}
 	}
 	// related_with_customer
-	if p.RelatedWithCustomer != 0 {
+	if v := p.RelatedWithCustomer; v > 0 {
 		paramsMap["params[0][related_with_customer]"] = strconv.FormatInt(p.RelatedWithCustomer, 10)
 	}
 
-	countFields := 0
 	// fields
-	for k, v := range p.Fields {
-		paramsMap[fmt.Sprintf("params[0][fields][%d][id]", countFields)] = strconv.FormatInt(k, 10)
-		paramsMap[fmt.Sprintf("params[0][fields][%d][value]", countFields)] = v
-		countFields++
+	fieldsCount := 0
+	for id, v := range p.Fields {
+		if id <= 0 || v == "" {
+			continue
+		}
+		// ID
+		paramsMap[fmt.Sprintf("params[0][fields][%d][id]", fieldsCount)] = strconv.FormatInt(id, 10)
+		// Value
+		paramsMap[fmt.Sprintf("params[0][fields][%d][value]", fieldsCount)] = v
+
+		fieldsCount++
 	}
-	for k, v := range p.FieldsPoint {
-		paramsMap[fmt.Sprintf("params[0][fields][%d][id]", countFields)] = strconv.FormatInt(k, 10)
-		paramsMap[fmt.Sprintf("params[0][fields][%d][value][lat]", countFields)] = strconv.FormatFloat(v.Lat, 'f', 10, 64)
-		paramsMap[fmt.Sprintf("params[0][fields][%d][value][lon]", countFields)] = strconv.FormatFloat(v.Lon, 'f', 10, 64)
-		countFields++
+	// fields (point)
+	for id, point := range p.FieldsPoint {
+		if id <= 0 || point == nil {
+			continue
+		}
+		// Получение и валидация координат
+		latStr, lonStr := point.StringLat(), point.StringLon()
+		if latStr == "" || lonStr == "" {
+			continue
+		}
+		// ID
+		paramsMap[fmt.Sprintf("params[0][fields][%d][id]", fieldsCount)] = strconv.FormatInt(id, 10)
+		// Value
+		paramsMap[fmt.Sprintf("params[0][fields][%d][value][lat]", fieldsCount)] = latStr
+		paramsMap[fmt.Sprintf("params[0][fields][%d][value][lon]", fieldsCount)] = lonStr
+
+		fieldsCount++
 	}
-	for k, fileNames := range p.FieldsFile {
-		for _, fileName := range fileNames {
-			paramsMap[fmt.Sprintf("params[0][fields][%d][id]", countFields)] = strconv.FormatInt(k, 10)
-			paramsMap[fmt.Sprintf("params[0][fields][%d][value]", countFields)] = fileName
-			countFields++
+	// fields (file)
+	for id, files := range p.FieldsFile {
+		if id <= 0 || len(files) == 0 {
+			continue
+		}
+		// Обработка слайсов имен файлов
+		for _, f := range files {
+			// ID
+			paramsMap[fmt.Sprintf("params[0][fields][%d][id]", fieldsCount)] = strconv.FormatInt(id, 10)
+			// Value
+			paramsMap[fmt.Sprintf("params[0][fields][%d][value]", fieldsCount)] = f
+
+			fieldsCount++
 		}
 	}
 
 	return paramsMap
+}
+
+// =====================================================================================================================
+// Response
+// =====================================================================================================================
+
+type StockInsertResponse struct {
+	Status  string  `json:"status,omitempty"`
+	Message string  `json:"message,omitempty"`
+	Data    []int64 `json:"data,omitempty"`
+}
+
+func (r *StockInsertResponse) GetErrorMessage() string {
+	switch {
+	case r == nil:
+		return ""
+	default:
+		return ""
+	case r.Status != "" && r.Message != "":
+		return r.Status + ": " + r.Message
+	case r.Message != "":
+		return r.Message
+	}
 }
